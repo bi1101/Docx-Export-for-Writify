bd972c433783be92e8801c2b89c12d63fd1d964b
bi1101 <bihungba1101@GMAIL.COM>
2023-10-28 18:01:54 +0700

    format

--- script.js
+++ script.js
@@ -1,131 +1,131 @@
 function extractRawCommentsFromHTML() {
     const vocabElements = document.querySelectorAll(".upgrade_vocab");
     const rawComments = [];
-  
+
     vocabElements.forEach((element) => {
-      const originalVocab = element.querySelector(".original-vocab").innerText;
-      const improvedVocab = element.querySelector(".improved-vocab").innerText;
-      const explanation = element.querySelector(".explanation").innerText;
-  
-      const commentData = {
-        originalVocab: originalVocab,
-        improvedVocab: improvedVocab,
-        explanation: explanation
-      };
-  
-      rawComments.push(commentData);
+        const originalVocab = element.querySelector(".original-vocab").innerText;
+        const improvedVocab = element.querySelector(".improved-vocab").innerText;
+        const explanation = element.querySelector(".explanation").innerText;
+
+        const commentData = {
+            originalVocab: originalVocab,
+            improvedVocab: improvedVocab,
+            explanation: explanation
+        };
+
+        rawComments.push(commentData);
     });
-  
+
     return rawComments;
-  }
-  
-  function convertRawCommentsToDocxFormat(rawComments) {
+}
+
+function convertRawCommentsToDocxFormat(rawComments) {
     return rawComments.map((comment, index) => ({
-      id: index,
-      author: "Teacher",
-      date: new Date(),
-      children: [
-        new docx.Paragraph({
-          children: [
-            new docx.TextRun({
-              text: comment.originalVocab + " -> " + comment.improvedVocab
+        id: index,
+        author: "Teacher",
+        date: new Date(),
+        children: [
+            new docx.Paragraph({
+                children: [
+                    new docx.TextRun({
+                        text: comment.originalVocab + " -> " + comment.improvedVocab
+                    })
+                ]
+            }),
+            new docx.Paragraph({}),
+            new docx.Paragraph({
+                children: [
+                    new docx.TextRun({ text: comment.explanation })
+                ]
             })
-          ]
-        }),
-        new docx.Paragraph({}),
-        new docx.Paragraph({
-          children: [
-            new docx.TextRun({ text: comment.explanation })
-          ]
-        })
-      ]
+        ]
     }));
-  }
-  
-  function createSectionsWithComments(rawComments) {
+}
+
+function createSectionsWithComments(rawComments) {
     const essayText = document.querySelector("#my-text").innerText;
     const essayPrompt = document.querySelector(".essay_prompt .elementor-widget-container").innerText.trim();
     const essayParagraphs = essayText.split(/\\r?\\n/).map(p => p.trimStart());
     const essayPromptParagraphs = essayPrompt.split(/\\r?\\n/).map(p => p.trimStart());
     const outputParagraphs = [];
-  
+
     // Add the essay prompt paragraphs to the output
     for (let promptParagraph of essayPromptParagraphs) {
-      if (promptParagraph.trim()) {  // Check if paragraph is not just whitespace
-        outputParagraphs.push(new docx.Paragraph({
-          children: [new docx.TextRun({
-            text: promptParagraph,
-            bold: true
-          })]
-        }));
-      }
-    }
-  
-    for (let paraText of essayParagraphs) {
-      if (paraText.trim()) {
-        // Check if paragraph is not just whitespace
-        let currentPosition = 0;
-        const paraChildren = [];
-        let localCommentIndex = 0; // Reset for each paragraph
-  
-        while (localCommentIndex < rawComments.length) {
-          const commentStartPos = paraText
-            .toLowerCase()
-            .indexOf(
-              rawComments[localCommentIndex].originalVocab.toLowerCase(),
-              currentPosition
-            );
-  
-          if (commentStartPos !== -1) {
-            // Add text before the comment
-            const preCommentText = paraText.slice(
-              currentPosition,
-              commentStartPos
-            );
-            paraChildren.push(new docx.TextRun(preCommentText));
-  
-            // Add the comment
-            paraChildren.push(new docx.CommentRangeStart(localCommentIndex));
-            paraChildren.push(
-              new docx.TextRun(rawComments[localCommentIndex].originalVocab)
-            );
-            paraChildren.push(new docx.CommentRangeEnd(localCommentIndex));
-            paraChildren.push(
-              new docx.TextRun({
-                children: [new docx.CommentReference(localCommentIndex)]
-              })
-            );
-  
-            currentPosition =
-              commentStartPos +
-              rawComments[localCommentIndex].originalVocab.length;
-  
-            localCommentIndex++;
-          } else {
-            console.warn(
-              `Skipped raw comment at index ${localCommentIndex} because it was not found in the essay text.`
-            );
-            // If no comment is found in the current paragraph, move on to the next comment
-            localCommentIndex++;
-            continue;
-          }
+        if (promptParagraph.trim()) {  // Check if paragraph is not just whitespace
+            outputParagraphs.push(new docx.Paragraph({
+                children: [new docx.TextRun({
+                    text: promptParagraph,
+                    bold: true
+                })]
+            }));
         }
-  
-        // Add the remaining part of the paragraph
-        const postCommentText = paraText.slice(currentPosition);
-        paraChildren.push(new docx.TextRun(postCommentText));
-  
-        outputParagraphs.push(new docx.Paragraph({ children: paraChildren }));
-      }
     }
-  
+
+    for (let paraText of essayParagraphs) {
+        if (paraText.trim()) {
+            // Check if paragraph is not just whitespace
+            let currentPosition = 0;
+            const paraChildren = [];
+            let localCommentIndex = 0; // Reset for each paragraph
+
+            while (localCommentIndex < rawComments.length) {
+                const commentStartPos = paraText
+                    .toLowerCase()
+                    .indexOf(
+                        rawComments[localCommentIndex].originalVocab.toLowerCase(),
+                        currentPosition
+                    );
+
+                if (commentStartPos !== -1) {
+                    // Add text before the comment
+                    const preCommentText = paraText.slice(
+                        currentPosition,
+                        commentStartPos
+                    );
+                    paraChildren.push(new docx.TextRun(preCommentText));
+
+                    // Add the comment
+                    paraChildren.push(new docx.CommentRangeStart(localCommentIndex));
+                    paraChildren.push(
+                        new docx.TextRun(rawComments[localCommentIndex].originalVocab)
+                    );
+                    paraChildren.push(new docx.CommentRangeEnd(localCommentIndex));
+                    paraChildren.push(
+                        new docx.TextRun({
+                            children: [new docx.CommentReference(localCommentIndex)]
+                        })
+                    );
+
+                    currentPosition =
+                        commentStartPos +
+                        rawComments[localCommentIndex].originalVocab.length;
+
+                    localCommentIndex++;
+                } else {
+                    console.warn(
+                        `Skipped raw comment at index ${localCommentIndex} because it was not found in the essay text.`
+                    );
+                    // If no comment is found in the current paragraph, move on to the next comment
+                    localCommentIndex++;
+                    continue;
+                }
+            }
+
+            // Add the remaining part of the paragraph
+            const postCommentText = paraText.slice(currentPosition);
+            paraChildren.push(new docx.TextRun(postCommentText));
+
+            outputParagraphs.push(new docx.Paragraph({ children: paraChildren }));
+        }
+    }
+
     return outputParagraphs;
-  }
-  
-  function exportDocument() {
+}
+
+function exportDocument() {
     const rawComments = extractRawCommentsFromHTML();
     const commentsForDocx = convertRawCommentsToDocxFormat(rawComments);
-  
+
     // Generating sections
     const sectionsChildren = [];
     sectionsChildren.push(...createSectionsWithComments(rawComments));
@@ -134,26 +134,26 @@ function extractRawCommentsFromHTML() {
     sectionsChildren.push(...createNormalSections("lexical-resource"));
     sectionsChildren.push(...createNormalSections("grammatical-range-accuracy"));
     sectionsChildren.push(...createNormalSections("sample-answer"));
-  
+
     const doc = new docx.Document({
-      comments: {
-        children: commentsForDocx
-      },
-      sections: [
-        {
-          properties: {},
-          children: sectionsChildren
-        }
-      ]
+        comments: {
+            children: commentsForDocx
+        },
+        sections: [
+            {
+                properties: {},
+                children: sectionsChildren
+            }
+        ]
     });
-  
+
     // Convert the document to a blob and save it
     docx.Packer.toBlob(doc).then((blob) => {
-      saveBlobAsDocx(blob);
+        saveBlobAsDocx(blob);
     });
-  }
-  
-  function createNormalSections(className) {
+}
+
+function createNormalSections(className) {
     const element = document.querySelector(`.${className} .elementor-widget-container .elementor-shortcode`);
     if (!element) {
         console.warn(`No element found with class name: ${className}`);
@@ -166,7 +166,7 @@ function extractRawCommentsFromHTML() {
             if (child.tagName === 'P') {
                 // For paragraph tags
                 sections.push(htmlParagraphToDocx(child.innerHTML));
-                console.log({innerHTML})
+                console.log({ innerHTML })
             } else if (child.tagName === 'OL' || child.tagName === 'UL') {
                 // For ordered or unordered lists
                 sections.push(bulletPointsToDocx(child.innerHTML));
@@ -175,9 +175,9 @@ function extractRawCommentsFromHTML() {
     });
 
     return sections;
-  }
-  
-  function saveBlobAsDocx(blob) {
+}
+
+function saveBlobAsDocx(blob) {
     const url = window.URL.createObjectURL(blob);
     const a = document.createElement("a");
     a.href = url;
@@ -185,11 +185,11 @@ function extractRawCommentsFromHTML() {
     document.body.appendChild(a);
     a.click();
     a.remove();
-  }
-  
-  // Event Listener
-  document.addEventListener("DOMContentLoaded", function () {
+}
+
+// Event Listener
+document.addEventListener("DOMContentLoaded", function () {
     document.getElementById("export-docx").addEventListener("click", function () {
-      exportDocument();
+        exportDocument();
     });
-  });
+});
